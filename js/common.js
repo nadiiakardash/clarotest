d5dae70454ecfd22d1a8c0bc0acf5a469d329bef
nadiiakardash <67142058+nadiiakardash@users.noreply.github.com>
2021-06-22 08:31:32 +0300

    update

--- js/common.js
+++ js/common.js
@@ -145,7 +145,7 @@ $(document).ready(function () {
 
 
 
-
+    /* NEW */
 
     if ($('*').is('#sortable')) {
 
@@ -212,7 +212,24 @@ $(document).ready(function () {
     $('#sortable .selectedcountry').last().css('border-bottom', '4px solid #c2e5f9');
 
 
+    $('#sortable th').click(function () {
+        if ($('#sortable .selectedcountry').last().prev().hasClass('selectedcountry')) {
+            $('#sortable .selectedcountry').last().css('border-bottom', '4px solid #c2e5f9');
+        } else {
+            $('#sortable .selectedcountry').css('border-bottom', 'none');
+        }
 
+
+
+
+
+
+
+
+
+
+
+    });
 
 
 
@@ -233,71 +250,3 @@ $(document).ready(function () {
 
 });
 
-
-// document.addEventListener('DOMContentLoaded', function () {
-//     const table = document.getElementById('sortable');
-//     const headers = table.querySelectorAll('th');
-//     const tableBody = table.querySelector('tbody');
-//     const rows = tableBody.querySelectorAll('tr');
-
-//     // Направление сортировки
-//     const directions = Array.from(headers).map(function (header) {
-//         return '';
-//     });
-
-//     // Преобразовать содержимое данной ячейки в заданном столбце
-//     const transform = function (index, content) {
-//         // Получить тип данных столбца
-//         const type = headers[index].getAttribute('data-type');
-//         switch (type) {
-//             case 'number':
-//                 return parseFloat(content);
-//             case 'string':
-//             default:
-//                 return content;
-//         }
-//     };
-
-//     const sortColumn = function (index) {
-//         // Получить текущее направление
-//         const direction = directions[index] || 'asc';
-
-//         // Фактор по направлению
-//         const multiplier = (direction === 'asc') ? 1 : -1;
-
-//         const newRows = Array.from(rows);
-
-//         newRows.sort(function (rowA, rowB) {
-//             const cellA = rowA.querySelectorAll('td')[index].innerHTML;
-//             const cellB = rowB.querySelectorAll('td')[index].innerHTML;
-
-//             const a = transform(index, cellA);
-//             const b = transform(index, cellB);
-
-//             switch (true) {
-//                 case a > b: return 1 * multiplier;
-//                 case a < b: return -1 * multiplier;
-//                 case a === b: return 0;
-//             }
-//         });
-
-//         // Удалить старые строки
-//         [].forEach.call(rows, function (row) {
-//             tableBody.removeChild(row);
-//         });
-
-//         // Поменять направление
-//         directions[index] = direction === 'asc' ? 'desc' : 'asc';
-
-//         // Добавить новую строку
-//         newRows.forEach(function (newRow) {
-//             tableBody.appendChild(newRow);
-//         });
-//     };
-
-//     [].forEach.call(headers, function (header, index) {
-//         header.addEventListener('click', function () {
-//             sortColumn(index);
-//         });
-//     });
-// });
